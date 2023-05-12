fd91a61f1b7b2bf057c6484f625ab1e267c5ce82
Gustavo Kauan <gustavokauan2002@hotmail.com>
2023-05-12 11:59:50 -0300

    no message

---
+++ CadastroLeitor/LeitorDAO.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroLeitor
+{
+    public class LeitorDAO
+    {
+        private SqlConnection Connection { get; }
+
+        public LeitorDAO(SqlConnection connection)
+        {
+            Connection = connection;
+        }
+
+        public void Salvar(LeitorModel leitor)
+        {
+            using (SqlCommand command = Connection.CreateCommand())
+            {
+                SqlTransaction t = Connection.BeginTransaction();
+                try
+                {
+                    StringBuilder sql = new StringBuilder();
+                    sql.AppendLine($"INSERT INTO mvtBibItemAcervo(nomeLeitor, sexo, dataNascimento, cpf) VALUES @nomeLeitor, @sexo," +
+                        $" @dataNascimento, @cpf");
+                    command.CommandText = sql.ToString();
+                    command.Parameters.Add(new SqlParameter("@codLocal", itemAcervo.CodLocal));
+
+                    command.Transaction = t;
+                    command.ExecuteNonQuery();
+                    t.Commit();
+                }
+                catch(Exception ex)
+                {
+                    t.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+        public void Editar(LeitorModel leitor)
+        {
+
+        }
+
+        public void Excluir(LeitorModel leitor, SqlTransaction t = null)
+        {
+            using (SqlCommand command = Connection.CreateCommand())
+            {
+                if (t != null)
+                {
+                    command.Transaction = t;
+                }
+                StringBuilder sql = new StringBuilder();
+                sql.AppendLine($"DELETE FROM mvtBibLeitor WHERE codLeitor = @codLeitor");
+                command.CommandText = sql.ToString();
+                command.Parameters.Add(new SqlParameter("@codLeitor", leitor.CodLeitor));
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public int VerificaRegistros(LeitorModel leitor)
+        {
+            using (SqlCommand command = Connection.CreateCommand())
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.AppendLine($"SELECT COUNT(codLeitor) FROM mvtBibLeitor WHERE codLeitor = @codLeitor");
+                command.CommandText = sql.ToString();
+                command.Parameters.AddWithValue("@codLeitor", leitor.CodLeitor);
+                int count = Convert.ToInt32(command.ExecuteScalar());
+                return count;
+            }
+        }
+
+        public List<LeitorModel> GetLeitores()
+        {
+            return null;
+        }
+
+        public LeitorModel PopulateDr(SqlDataReader dr)
+        {
+            return null;
+        }
+    }
+}
+}
---
+++ CadastroLeitor/LeitorModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroLeitor
+{
+    public class LeitorModel
+    {
+        public string CodLeitor { get; set; }
+
+        public string NomeLeitor { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string DataNascimento { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Rg { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string TelefoneCelular { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string EnderecoBairro { get; set; }
+
+        public string EnderecoCidade { get; set; }
+
+        public string EnderecoCep { get; set; }
+
+        public string EnderecoUf { get; set; }
+
+        public string EnderecoNumero { get; set; }
+    }
+}
